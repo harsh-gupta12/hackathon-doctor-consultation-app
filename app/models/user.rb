f691856f58d3bdc1e0bba57d44fb8f110a7e33f7
Harsh Gupta <harsh@Harshs-MacBook-Pro.local>
2022-08-18 00:10:15 +0530

    Lint Errors Resolved

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    has_secure_password
-  
-    validates :name, presence: true
-    validates :email, presence: true, uniqueness: true
-    validates :mobile, presence: true, uniqueness: true
-    validates :username, presence: true, uniqueness: true
-  end
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :mobile, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+end
